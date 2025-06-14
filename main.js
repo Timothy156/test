f2669b8aa79c0d810796d8bba4eda73ff24e8807
Timothy156 <tmthy156.official@gmail.com>
2025-06-14 19:55:50 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 console.log(12+12);
-alert("It's working");
+const a = document.createElement('div');
+a.style.width = '100px';
+a.style.height = '100px';
+a.style.backgroundColor = 'blue';
+const b = document.createElement('button');
+b.textContent = 'Test';
+a.appendChild(b);
+document.body.appendChild(a);
